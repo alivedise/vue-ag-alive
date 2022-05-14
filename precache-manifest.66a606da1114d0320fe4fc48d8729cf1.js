a5022b06134b4a43ba9b549d0210ef2597ad547b
Alive.Kuo <alegnadise@gmail.com>
2022-05-14 13:00:10 +0800

    deploy

---
+++ precache-manifest.66a606da1114d0320fe4fc48d8729cf1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fea7e85f417348b019",
+    "url": "/vue-ag-alive/css/chunk-vendors.cf41d1f5.css"
+  },
+  {
+    "revision": "9f00e50d632d640dbff3",
+    "url": "/vue-ag-alive/css/revive.0a21076d.css"
+  },
+  {
+    "revision": "45c0a1f58b72dce2f8c47be546774dbc",
+    "url": "/vue-ag-alive/icon.png"
+  },
+  {
+    "revision": "560d2d64e7380659dfeebbd6f5ff2b0d",
+    "url": "/vue-ag-alive/index.html"
+  },
+  {
+    "revision": "11604467116ac9944d31",
+    "url": "/vue-ag-alive/js/app.83f25814.js"
+  },
+  {
+    "revision": "a7fea7e85f417348b019",
+    "url": "/vue-ag-alive/js/chunk-vendors.f08dad44.js"
+  },
+  {
+    "revision": "9f00e50d632d640dbff3",
+    "url": "/vue-ag-alive/js/revive.8514d55f.js"
+  },
+  {
+    "revision": "bf2d42af528fab6d6a3e08c27d6e32d3",
+    "url": "/vue-ag-alive/logo.png"
+  },
+  {
+    "revision": "6bb2dfec5a77086191f5097126daf1c5",
+    "url": "/vue-ag-alive/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-ag-alive/robots.txt"
+  }
+]);
