c037a76b4fdc6d18d571ffdac4ce4fabe45f54b1
Alive.Kuo <alegnadise@gmail.com>
2022-05-14 12:38:22 +0800

    fix(app): fix hint v2

--- src/components/EquipmentCalculator.vue
+++ src/components/EquipmentCalculator.vue
@@ -185,8 +185,8 @@
       >
         <v-text-field
           v-model="fast"
-          label="快速輸入(範例:a10 ap10.9 c10.9 x20.1)可不照順序 有小數一定要輸入 不用打%"
-          hint="a攻擊 a攻擊% c爆擊 x暴傷 s速度 h生命 h生命% d防禦 d防禦% t命中 r抵抗"
+          label="快速輸入(範例:a10 a10.9 c10.9 x20.1)=(固定攻擊10 百分攻擊10.9 爆擊10.9 暴傷20.1)可不照順序 有小數一定要輸入 不用打%"
+          hint="a攻擊/攻擊% c爆擊 x暴傷 s速度 h生命/生命% d防禦/防禦% t命中 r抵抗"
           @change="tryParse"
           filled
         ></v-text-field>
